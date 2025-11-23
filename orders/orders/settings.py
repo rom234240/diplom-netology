9c7dc94ad469804015322de2a83aafce7a7064ad
Sergey Romanov <serhromanov23@gmail.com>
2025-11-23 22:58:50 +0300

    Добавил приложения

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User model
+
+AUTH_USER_MODEL = 'users.User'
