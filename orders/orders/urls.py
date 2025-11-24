62315a3a7b757f6871f6db93b91f5027f34c4b5f
Sergey Romanov <serhromanov23@gmail.com>
2025-11-24 19:14:37 +0300

    Обновил orders/urls.py

--- orders/orders/urls.py
+++ orders/orders/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .admin import admin_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
