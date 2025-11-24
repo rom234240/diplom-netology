a3d85897854d7edfadf6ae80d23d201ecdca6ac9
Sergey Romanov <serhromanov23@gmail.com>
2025-11-24 18:38:59 +0300

    Зарегистрировал модели users/admin.py

--- orders/users/admin.py
+++ orders/users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'company', 'position', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'company')
+    ordering = ('email')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal_info'), {'fields': ('first_name', 'last_name', 'company', 'position')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important_dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes':('wide'),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'company', 'position'),
+        }),
+    )
+    
