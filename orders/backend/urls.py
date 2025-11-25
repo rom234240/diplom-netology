4683ceb079dbe7cf43beacc4e9d25b2c04582878
Sergey Romanov <serhromanov23@gmail.com>
2025-11-25 22:47:27 +0300

    Добавил URL для списка товаров

--- orders/backend/urls.py
+++ orders/backend/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PartnerUpdate, RegisterView, LoginView
+from .views import PartnerUpdate, RegisterView, LoginView, ProductListView
 
 
 urlpatterns = [
     path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
     path('user/register', RegisterView.as_view(), name='user-register'),
     path('user/login', LoginView.as_view(), name='user-login'),
+    path('products', ProductListView.as_view(), name='product-list'),
 ]
