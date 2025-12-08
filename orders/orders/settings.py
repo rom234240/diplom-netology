6fc94544042215130858389fa027683ff2e681c6
Sergey Romanov <serhromanov23@gmail.com>
2025-12-08 19:09:41 +0300

    Добавил настройки celery

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -168,6 +168,17 @@ REST_FRAMEWORK = {
     ],
 }
 
+# Настройки Celery
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
 # Настройки email
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
