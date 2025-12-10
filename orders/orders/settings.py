bd8323be3f299a9261a7635b58ee8cc933e898d6
Sergey Romanov <serhromanov23@gmail.com>
2025-12-10 19:45:38 +0300

    Добавил middleware

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -96,7 +96,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware'
+    'social_django.middleware.SocialAuthExceptionMiddleware',
+
+    'backend.middleware.CacheMetricsMiddleware',
+    'backend.middleware.QueryCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'orders.urls'
