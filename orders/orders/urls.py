a47d03bb4ccb1a4ce3e66196f0168b0e9d846894
Sergey Romanov <serhromanov23@gmail.com>
2025-12-11 20:44:55 +0300

    Добавил serving медиа

--- orders/orders/urls.py
+++ orders/orders/urls.py
@@ -27,11 +27,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
 
 
 # Конфигурация схемы OpenAPI/Swagger для автоматической генерации документации
@@ -76,3 +78,6 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='home'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
